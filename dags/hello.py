0215c5014744b58345425f803a6c92b92653312e
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-05 19:21:46 +0100

    hello dag

---
+++ dags/hello.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators import PythonOperator
+
+# Simple function for the task
+def hello():
+    print("Airflow is up and!")
+
+with DAG(
+    dag_id="test_simple_dag",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,  # Run manually
+    catchup=False,
+) as dag:
+
+    test_task = PythonOperator(
+        task_id="print_message",
+        python_callable=hello
+    )
