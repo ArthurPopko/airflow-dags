ca0fb50d7564882eff320ef88a84aef41d05e54e
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-12 00:41:57 +0100

    s3 list files

--- dags/nyc_stats.py
+++ dags/nyc_stats.py
@@ -3,25 +3,27 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
 
-def list_files_from_s3():
-    # Используем Connection с aws_conn_id="my_aws_conn"
-    hook = S3Hook(aws_conn_id="aws_default")
-    
-    # Получаем список файлов в бакете с префиксом
-    keys = hook.list_keys(bucket_name="nyc-tlc", prefix="trip data/")
-    
-    # Выводим первые 50 файлов
-    for k in keys[:50]:
-        print(k)
+def check_aws_connection():
+    try:
+        # Используем Connection aws_default (или своё имя)
+        hook = S3Hook(aws_conn_id="aws_default")
+        
+        # Просто проверяем, можем ли получить список бакетов
+        buckets = hook.list_buckets()
+        print("Connected to AWS successfully!")
+        print("Buckets available:", buckets)
+    except Exception as e:
+        print("AWS connection failed:", e)
+        raise
 
 with DAG(
-    dag_id="list_nyc_tlc_files",
-    start_date=datetime(2024, 1, 1),
+    dag_id="check_aws_connection",
+    start_date=datetime(2025, 11, 11),
     schedule=None,
     catchup=False
 ) as dag:
 
     PythonOperator(
-        task_id="list_files",
-        python_callable=list_files_from_s3
+        task_id="check_connection",
+        python_callable=check_aws_connection
     )
