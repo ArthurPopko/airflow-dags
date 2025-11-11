a411c77bed871dd64ac2b9e4756b83292e8179a7
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-11 23:31:28 +0100

    s3 list files

--- dags/nyc_stats.py
+++ dags/nyc_stats.py
@@ -1,26 +1,26 @@
-import boto3
-from botocore import UNSIGNED
-from botocore.config import Config
-
 from airflow import DAG
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
 
 def list_files_from_s3():
-    s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
-    resp = s3.list_objects_v2(
-        Bucket="nyc-tlc",
-        Prefix="trip data/"
-    )
-    for obj in resp.get("Contents", [])[:50]:
-        print(obj["Key"])
+    # Используем Connection с aws_conn_id="my_aws_conn"
+    hook = S3Hook(aws_conn_id="my_aws_conn")
+    
+    # Получаем список файлов в бакете с префиксом
+    keys = hook.list_keys(bucket_name="nyc-tlc", prefix="trip data/")
+    
+    # Выводим первые 50 файлов
+    for k in keys[:50]:
+        print(k)
 
 with DAG(
     dag_id="list_nyc_tlc_files",
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False
-):
+) as dag:
+
     PythonOperator(
         task_id="list_files",
         python_callable=list_files_from_s3
