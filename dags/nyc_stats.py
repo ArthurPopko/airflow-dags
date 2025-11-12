852582756f902fbaac712db313cb6cc7b95707d4
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-12 01:03:04 +0100

    s3 list files

--- dags/nyc_stats.py
+++ dags/nyc_stats.py
@@ -4,41 +4,45 @@ from airflow.hooks.base import BaseHook
 from datetime import datetime
 import boto3
 
-def check_aws_connection_from_conn():
+def list_nyc_tlc_files():
     try:
-        # Получаем креды из Airflow Connection
         conn = BaseHook.get_connection("aws_default")
         aws_access_key = conn.login
         aws_secret_key = conn.password
         region_name = conn.extra_dejson.get("region_name", "us-east-1")
 
-        # Создаём boto3 клиент STS с этими кредами
-        sts = boto3.client(
-            "sts",
+        s3 = boto3.client(
+            "s3",
             aws_access_key_id=aws_access_key,
             aws_secret_access_key=aws_secret_key,
             region_name=region_name
         )
 
-        # Проверяем соединение
-        identity = sts.get_caller_identity()
-        print("Connected to AWS successfully!")
-        print("Account:", identity["Account"])
-        print("UserId:", identity["UserId"])
-        print("ARN:", identity["Arn"])
+        resp = s3.list_objects_v2(
+            Bucket="nyc-tlc",
+            Prefix="trip data/"
+        )
+
+        contents = resp.get("Contents", [])
+        if not contents:
+            print("No files found in bucket.")
+            return
+
+        for obj in contents[:50]:
+            print(obj["Key"])
 
     except Exception as e:
-        print("AWS connection failed:", e)
+        print("Failed to list files:", e)
         raise
 
 with DAG(
-    dag_id="check_aws_connection_boto3_conn",
+    dag_id="list_nyc_tlc_files",
     start_date=datetime(2025, 11, 12),
     schedule=None,
     catchup=False
 ) as dag:
 
     PythonOperator(
-        task_id="check_connection_boto3_conn",
-        python_callable=check_aws_connection_from_conn
+        task_id="list_files",
+        python_callable=list_nyc_tlc_files
     )
