526487f326100fe0eb25ed166287402e3ed3dd26
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-12 20:07:00 +0100

    ca driver removed

--- dags/nyc_stats.py
+++ dags/nyc_stats.py
@@ -9,7 +9,6 @@ DAG_CONFIG = Variable.get(f"{DAG_ID.lower()}__config", {}, deserialize_json=True
 
 AWS_REGION = DAG_CONFIG.get("AWS_REGION")
 S3_BUCKET = DAG_CONFIG.get("S3_BUCKET")
-S3_PREFIX = DAG_CONFIG.get("S3_PREFIX")
 BASE_URL = DAG_CONFIG.get("BASE_URL")
 MONTHS = DAG_CONFIG.get("MONTHS")
 CAB_TYPES = DAG_CONFIG.get("CAB_TYPES")
@@ -33,7 +32,7 @@ with DAG(
         for month in MONTHS:
             file_name = f"{cab}_tripdata_{month}.parquet"
             url = f"{BASE_URL}/{file_name}"
-            s3_path = f"s3://{S3_BUCKET}/{S3_PREFIX}/{file_name}"
+            s3_path = f"s3://{S3_BUCKET}/{file_name}"
 
             stream_to_s3 = BashOperator(
                 task_id=f"stream_{cab}_{month}_to_s3",
