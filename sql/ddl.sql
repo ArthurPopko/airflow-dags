08b2f4911aab090f6e9fb31d5edf50a2c760dff5
Atrur <atrur@Atrurs-MacBook-Air.local>
2025-11-15 06:52:02 +0100

    df for clickhouse

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,31 +1,30 @@
-CREATE DATABASE IF NOT EXISTS staging;
-
 CREATE OR REPLACE TABLE staging.nyc_tlc_tripdata_local (
-    driver_id UInt32,
-    pu_location_id UInt32,
-    do_location_id UInt32,
-    vendor_id UInt32,
-    ratecode_id UInt32,
-    pickup_datetime DateTime64(3, 'UTC'),
-    dropoff_datetime DateTime64(3, 'UTC'),
-    trip_distance Float32,
-    fare_amount Float32,
-    total_amount Float32,
-    extra Float32,
-    mta_tax Float32,
-    tip_amount Float32,
-    tolls_amount Float32,
-    ehail_fee Float32,
-    improvement_surcharge Float32,
-    congestion_surcharge Nullable(Float32),
-    cbd_congestion_fee Float32,
-    passenger_count Nullable(UInt8),
-    payment_type UInt8,
-    trip_type UInt8,
-    cab_type String,
-    store_and_fwd_flag Nullable(String),
-    _etl_timestamp DateTime DEFAULT now()
-) ENGINE = ReplacingMergeTree(_etl_timestamp)
-PARTITION BY toYYYYMM(pickup_datetime)
-ORDER BY (driver_id, pickup_datetime)
+    driver_id UInt32,                             -- сгенерированный ID водителя
+    pickup_datetime DateTime64(3, 'UTC'),         -- время начала поездки
+    dropoff_datetime DateTime64(3, 'UTC'),        -- время конца поездки
+    passenger_count Nullable(UInt8),              -- кол-во пассажиров, может быть пустое
+    trip_distance Float32,                        -- расстояние поездки
+    pu_location_id UInt32,                         -- ID зоны посадки
+    do_location_id UInt32,                         -- ID зоны высадки
+    fare_amount Float32,                           -- стоимость поездки
+    total_amount Float32,                          -- общая сумма с чаевыми и сборами
+    cab_type String,                               -- "yellow" / "green"
+    vendor_id UInt32,                              -- ID компании-владельца
+    store_and_fwd_flag Nullable(String),          -- флаг сохранения и пересылки данных
+    ratecode_id UInt32,                            -- код тарифа
+    extra Float32,                                 -- дополнительные сборы
+    mta_tax Float32,                               -- налог MTA
+    tip_amount Float32,                            -- чаевые
+    tolls_amount Float32,                           -- сборы за платные дороги
+    ehail_fee Float32,                             -- сбор за электронный заказ (может быть 0)
+    improvement_surcharge Float32,                 -- дополнительная плата
+    payment_type UInt8,                            -- тип оплаты (нал, карта, прочее)
+    trip_type UInt8,                               -- тип поездки
+    congestion_surcharge Nullable(Float32),       -- плата за пробки, может быть пустой
+    cbd_congestion_fee Float32,                   -- плата за въезд в центр
+    _etl_timestamp DateTime DEFAULT now()         -- время вставки в таблицу
+)
+ENGINE = ReplacingMergeTree(_etl_timestamp)       -- позволяет заменять дубликаты
+PARTITION BY toYYYYMM(pickup_datetime)           -- партицирование по месяцу
+ORDER BY (driver_id, pickup_datetime)           -- сортировка для оптимизации по водителю и времени
 SETTINGS index_granularity = 8192;
