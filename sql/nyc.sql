0715857e61f9b47c02454b7574ad651f5b27a2f1
Artur <atrur@Atrurs-MacBook-Air.local>
2025-11-16 13:30:58 +0100

    df for clickhouse

--- sql/nyc.sql
+++ sql/nyc.sql
@@ -1,34 +1,7 @@
 -- count
 select count() from staging.nyc_tlc_tripdata_local;
 
-optimize table staging.nyc_tlc_tripdata_local;
-
-TRUNCATE TABLE staging.nyc_tlc_tripdata_local;
+optimize table staging.nyc_tlc_tripdata_local FINAL DEDUPLICATE by driver_id,
+pickup_datetime;
 
 select * from staging.nyc_tlc_tripdata_local limit 10;
-
-SELECT
-    driver_id,
-    min(pickup_datetime) AS shift_start,
-    max(dropoff_datetime) AS shift_end,
-    dateDiff(
-        'day',
-        shift_start,
-        shift_end
-    ) AS shift_time_min
-FROM staging.nyc_tlc_tripdata_local
-GROUP BY
-    driver_id;
-;
-
-select
-    driver_id,
-    sum(trip_distance * 1.60934) AS total_distance_km,
-    sum(fare_amount) as total_fare,
-    count() as trip_count
-from staging.nyc_tlc_tripdata_local
-group by
-    driver_id
-order by driver_id;
-
-SELECT DISTINCT cab_type FROM staging.nyc_tlc_tripdata_local;
