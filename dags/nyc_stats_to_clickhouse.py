a3f433ced8a3770fd00543d67335ae7e354a6de2
Artur <atrur@Atrurs-MacBook-Air.local>
2025-11-23 14:33:46 +0100

    slack

--- dags/nyc_stats_to_clickhouse.py
+++ dags/nyc_stats_to_clickhouse.py
@@ -53,6 +53,51 @@ COLUMNS = [
 ]
 
 
+def build_slack_message(
+    dag_id: str,
+    task_id: str,
+    log_url: str,
+    file_path: str,
+    inserted_rows: int,
+) -> str:
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    return f"""
+                📊 *Insert into ClickHouse Completed Successfully!*
+
+                *DAG:* `{dag_id}`
+                *Task:* `{task_id}`
+                *File:* `{file_path}`
+                *Table:* `{SCHEMA}.{TABLE}`
+
+                *Total Rows:* *{inserted_rows}*
+                *Timestamp:* {timestamp}
+
+                🔗 *Logs:* {log_url}
+
+                Everything looks good! 🎉
+            """
+
+
+def send_slack_message(message: str):
+    conn = BaseHook.get_connection(SLACK_CONN_ID)
+    token = conn.password
+
+    logging.info("Sending message to Slack...")
+
+    resp = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": f"Bearer {token}"},
+        data={
+            "channel": SLACK_CHANNEL,
+            "text": message,
+        },
+    )
+
+    if not resp.ok or not resp.json().get("ok"):
+        logging.error(f"Failed to send message to Slack: {resp.text}")
+
+
 @task(retries=3, retry_delay=timedelta(seconds=30))
 def download_file(cab: str, month: str):
     os.makedirs(LOCAL_DIR, exist_ok=True)
@@ -138,46 +183,49 @@ def prepare_month(file_path: str):
 
 
 @task(retries=2, retry_delay=timedelta(seconds=10))
-def insert_month(file_path: str):
+def insert_month(file_path: str, **kwargs):
     logging.info(f"[LOAD] Loading file {file_path} into ClickHouse")
-    # conn = BaseHook.get_connection("click")
-    # client = Client(
-    #     host=conn.host,
-    #     port=int(conn.port or 9000),
-    #     user=conn.login,
-    #     password=conn.password,
-    #     database=conn.schema or SCHEMA,
-    # )
+    conn = BaseHook.get_connection("click")
 
-    # df = pd.read_parquet(file_path)
-    # cols = [col for col in COLUMNS if col in df.columns]
-
-    # batch_size = BATCH_SIZE
-    # total_rows = len(df)
-    # logging.info(f"[LOAD] Total rows to insert: {total_rows}")
-    # for start in range(0, total_rows, batch_size):
-    #     batch = df.iloc[start : start + batch_size].to_dict("records")
-    #     client.execute(
-    #         f"INSERT INTO {SCHEMA}.{TABLE} ({', '.join(cols)}) VALUES", batch
-    #     )
-    #     logging.info(f"[LOAD] Inserted rows {start}-{start + len(batch)}")
-
-    conn = BaseHook.get_connection(SLACK_CONN_ID)
-    token = conn.password
-
-    logging.info("Sending msg to Slack...")
-    title = "Insert Successful"
-    resp = requests.post(
-        "https://slack.com/api/chat.postMessage",
-        headers={"Authorization": f"Bearer {token}"},
-        data={
-            "channel": SLACK_CHANNEL,
-            "title": title,
-            "markdown_text": "**Hello world**",
-        },
+    client = Client(
+        host=conn.host,
+        port=int(conn.port or 9000),
+        user=conn.login,
+        password=conn.password,
+        database=conn.schema or SCHEMA,
     )
-    if not resp.ok or not resp.json().get("ok"):
-        logging.error(f"Failed to send msg to Slack: {resp.text}")
+
+    df = pd.read_parquet(file_path)
+    cols = [col for col in COLUMNS if col in df.columns]
+
+    batch_size = BATCH_SIZE
+    total_rows = len(df)
+    inserted_rows = 0
+
+    logging.info(f"[LOAD] Total rows to insert: {total_rows}")
+
+    for start in range(0, total_rows, batch_size):
+        batch = df.iloc[start : start + batch_size].to_dict("records")
+        client.execute(
+            f"INSERT INTO {SCHEMA}.{TABLE} ({', '.join(cols)}) VALUES", batch
+        )
+        inserted_rows += len(batch)
+        logging.info(f"[LOAD] Inserted rows {start}-{start + len(batch)}")
+
+    ti = kwargs["task_instance"]
+    dag_id = ti.dag_id
+    task_id = ti.task_id
+    log_url = ti.log_url
+
+    slack_message = build_slack_message(
+        dag_id=dag_id,
+        task_id=task_id,
+        log_url=log_url,
+        file_path=file_path,
+        inserted_rows=inserted_rows,
+    )
+
+    send_slack_message(slack_message)
 
 
 with DAG(
@@ -189,11 +237,10 @@ with DAG(
     tags=["nyc", "etl"],
 ) as dag:
 
-    # for cab in CAB_TYPES:
-    #     for month in MONTHS:
-    #         with TaskGroup(group_id=f"{cab}_{month}") as tg:
-    #             download = download_file(cab, month)
-    #             clean = prepare_month(download)
-    #             insert = insert_month(clean)
-    #             download >> clean >> insert
-    insert_month("123")
+    for cab in CAB_TYPES:
+        for month in MONTHS:
+            with TaskGroup(group_id=f"{cab}_{month}") as tg:
+                download = download_file(cab, month)
+                clean = prepare_month(download)
+                insert = insert_month(clean)
+                download >> clean >> insert
